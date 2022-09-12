011eeae213f117d649acddf6d6b094a5eca87aff
michael <michael@vrazel.net>
2022-09-12 09:28:51 -0600

    Added copyright notice per GPL v3

--- Windows.Minecraft.Service/Properties/AssemblyInfo.cs
+++ Windows.Minecraft.Service/Properties/AssemblyInfo.cs
@@ -1,4 +1,20 @@
-﻿using System.Reflection;
+﻿// Windows.Minecraft.Service - A simple Windows Service wrapper for Minecraft Server
+// Copyright (C) 2022 Michael Vrazel
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +22,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Windows.Minecraft.Service")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple Windows Service wrapper for Minecraft Server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("beamship.com")]
+[assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("Windows.Minecraft.Service")]
-[assembly: AssemblyCopyright("Copyright © beamship.com 2022")]
+[assembly: AssemblyCopyright("Copyright © 2022 Michael Vrazel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
